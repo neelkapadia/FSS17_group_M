54ed2e7794fbe53f2926df9ff35cfde80a266ee2
KonigSats <iamsats@gmail.com>
2017-09-21 02:13:54 -0400

    Unspervised learner check-in

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -7,40 +7,43 @@ total_values=50
 def getList():
     op=[]
     for i in range(0,total_values):
-        op.append(decimal.Decimal(random.random()))
+        op.append(float(decimal.Decimal(random.random())))
 
-    for i in op:
-        print(i)
     return op
 
 def getUnsupervisedBins(num_column):
     x_bin=[]
-    bin_size=math.sqrt(total_values)+1
+    bin_size=math.ceil(math.sqrt(total_values))+1
     bin_size_counter=0
     unsuper_dict={}
     dict_index=1
     num_column_index=0
     
-    while ((bin_size_counter/total_values)<=(bin_size)) and (num_column_index<len(num_column)):
-        key="Label "+dict_index;
-        xbin.append(num_column[num_column_index])
+    while (bin_size_counter<bin_size) and (num_column_index<len(num_column)):
+        key="Label"+str(dict_index);
+        x_bin.append(num_column[num_column_index])
         num_column_index=num_column_index+1
         bin_size_counter=bin_size_counter+1
-        if (bin_size_counter/total_values)==bin_size:
-            unsuper_dict = {key:xbin}
+        if (bin_size_counter==bin_size-1) or (num_column_index==len(num_column)):
+            unsuper_dict[key] = x_bin
+            x_bin=[]
+
             bin_size_counter=0
             dict_index=dict_index+1
 
-    if(len(unsuper_dict.get("Label "+dict_index)) < bin_size):
-        last_index=dict_index
+    last_index=len(unsuper_dict)
+    if(len(unsuper_dict["Label"+str(last_index)]) < bin_size):        
         second_last_index = last_index-1
-        unsuper_dict["Label "+last_index].append(unsuper_dict.get("Label "+second_last_index))
-        del unsuper_dict["Label "+last_index]
+        unsuper_dict["Label"+str(second_last_index)] = unsuper_dict["Label"+str(second_last_index)] + unsuper_dict["Label"+str(last_index)]
+        del unsuper_dict["Label"+str(last_index)]
         
     return unsuper_dict
 
 num_column=[]
-num_column = getList().sort()
+unsuper_dict = {}
+num_column = getList()
+num_column.sort()
 unsuper_dict = getUnsupervisedBins(num_column)
+print("Unsupervised learner : ")
 for k, v in unsuper_dict.iteritems():
     print k, v
